9f4afd4797ef371932c32c9f5382e46578582d20
Mwattanachant <s6330613013@phuket.psu.ac.th>
2022-03-07 15:37:32 +0700

    Computer security lab1

--- Lab3/Q2.py
+++ Lab3/Q2.py
@@ -8,8 +8,6 @@ n = 1
 def input1(x):
     data = "\x00" * int(x / 8)
     data = data.encode()
-    # h1 = hashlib.sha256(data)
-    # st = h1.hexdigest()
     return data
 
 
@@ -17,7 +15,8 @@ def collision(st, v, bit):
     while True:
         h2 = hashlib.sha256(bytes(v))
         st2 = h2.digest()
-        if st[0:int(bit / 4)] == st2[0:int(bit / 4)]:
+        # print(st2)
+        if st[0:int(bit / 8)] == st2[0:int(bit / 8)]:
             print(bit, "- bit")
             print(v)
             print("hash1: ", st, "hash 2:", st2)
@@ -26,7 +25,8 @@ def collision(st, v, bit):
             v = v + 1
 
 
-st = input1(8)
-collision(st, n, 8)
+m1 = input1(8)
+collision(m1, n, 8)
 end_time = time.time()
 print("Time:", end_time - start_time)
+
