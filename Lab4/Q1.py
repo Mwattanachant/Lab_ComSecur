dfb6614abba1d65da86fc0c6fe6c4b3113ce6ecc
Mwattanachant <s6330613013@phuket.psu.ac.th>
2022-03-14 14:37:55 +0700

    Computer security lab4 Question 1 Update

--- Lab4/Q1.py
+++ Lab4/Q1.py
@@ -1,26 +1,28 @@
-n = 0
-generator = []
-
 
 def is_generator(g, p):
     x = []
-    check = False
-    print("g:", g)
-    for i in range(p - 1):
+    for i in range(p):
         x.append((g ** i) % p)
-        print(g, "^", i, "=", x[i])
-    for j in range(1, p - 1):
-        if x[0] == x[j]:
-            check = False
-            break
-        elif x[0] != x[j]:
-            check = True
-    return check
+    for j in range(1, p - 2):
+        if j != 0:
+            if x[0] == x[j]:
+                return False
+    return True
 
 
-for k in range(1, 3):
-    if is_generator(k, 127):
-        n = n + 1
-        generator.append(k)
-print("Number of generators: ", n)
-print("All generator:", generator)
+def defineGen(prime):
+    n = 0
+    generator = []
+    for k in range(1, prime):
+        if is_generator(k, prime):
+            n = n + 1
+            generator.append(k)
+    print("Number of generators: ", n)
+    print("All generator:", generator)
+
+
+defineGen(23)
+defineGen(127)
+# defineGen(27)
+
+
